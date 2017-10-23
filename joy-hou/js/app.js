0c058f6c1f5816d05115fbd0a13f3030bfb03f3c
strawbee <yuejiao.hou@gmail.com>
2017-10-23 11:25:47 -0700

    template literal part

--- joy-hou/js/app.js
+++ joy-hou/js/app.js
@@ -16,7 +16,7 @@ const votes = [];
 
 function Product(name) {
   this.name = name;
-  this.path = 'img/' + name + '.jpg';
+  this.path = `img/${name}.jpg`;
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -63,8 +63,8 @@ function handleClick(event) {
       if(event.target.id === allProducts[i].name) {
         allProducts[i].votes += 1;
         allProducts[i].votes += 1;
-        console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
-        console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+        console.log(`${event.target.id} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`);
+        console.log(`${event.target.id} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`);
       }
     }
   }
@@ -76,7 +76,7 @@ function handleClick(event) {
 function showList() {
   for(let i = 0; i < allProducts.length; i++) {
     const liEl = document.createElement('li');
-    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+    liEl.textContent = `${allProducts[i].name} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`;
     list.appendChild(liEl);
   }
 }
